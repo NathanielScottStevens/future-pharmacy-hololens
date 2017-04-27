9c6c8ddf111641bafc022f41c766840806e4e0f3
nathanielscottstevens <nathaniel.scott.stevens@gmail.com>
2017-04-26 21:50:23 -0400

    Initial Commit.

---
+++ Assets/Scripts/TapToPlaceInSphere.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using HoloToolkit.Unity.InputModule;
+
+public class TapToPlaceInSphere : MonoBehaviour, IInputClickHandler
+{
+    public float distanceFromUser = 2f;
+
+    bool isBeingPlaced = false;
+
+    private void Update()
+    {
+        if (isBeingPlaced)
+        {
+            Ray ray = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
+            transform.position = ray.GetPoint(distanceFromUser);
+        }
+    }
+
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        isBeingPlaced = !isBeingPlaced;
+    }
+}
